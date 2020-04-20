af56ce2b9e8f45f6312e0f930281d07387d41fdd
RotFrost <ma.hester@gmx.net>
2020-04-21 00:19:05 +0200

    Added code to train a lda model

--- first_cleanup.py
+++ first_cleanup.py
@@ -4,15 +4,20 @@ Created on Fri Apr 10 22:59:00 2020
 
 @author: mahes
 """
-
+import string
+import os
 import pandas as pd
 
-from nltk.tokenize import sent_tokenize, word_tokenize, wordpunct_tokenize
-from nltk.corpus import stopwords
-import string
+#from nltk.tokenize import sent_tokenize, word_tokenize, wordpunct_tokenize
+#from nltk.corpus import stopwords
+
 import spacy
 import de_core_news_sm
 nlp = de_core_news_sm.load()
+
+import gensim
+
+import pickle
 
 def isStopPunctSpace(token):
     return token.is_punct or token.is_space or token.is_stop
@@ -23,29 +28,71 @@ def combineStrings(df, row):
 def removeImagePart(richTextString):
     return richTextString.split('Foto:')[0] if len(richTextString) < 100 else richTextString
 
+def saveAsJson(dfTemp, filename):
+    with open(getDataPath(filename), 'w', encoding='utf-8') as file:
+        dfTemp.to_json(file, force_ascii=False)
+        
+def saveModel(model, modelname):
+    with open(getModelPath(modelname), 'wb') as f:
+        pickle.dump(LDAvis_prepared, f)
+        
+def loadModel(modelname):
+    with open(getModelPath(modelname), 'rb') as f:
+        return pickle.load(f)
+        
+def createStopPunctLemma(text):
+    doc = nlp(text)
+    tokens = []
+    for token in doc:
+        if not isStopPunctSpace(token):
+            tokens.append(token.lemma_)
+    return tokens
+
+def getDataPath(filename):
+    return os.path.join('data', filename)
+
+def getModelPath(modelname):
+    return os.path.join('model', modelname)
+
 richTextFilter = ['Zur externen Meldung', 'Zur externen Pressemitteilung']
 
-path = 'press42.json'
 
-df = pd.read_json(path)
+#Combine the columns 'shortText' and 'richText'. What is with the title?
+if False:
+    path = getDataPath('press42.json')
+    df = pd.read_json(path)
+    df['combText'] = df.apply(lambda row: df['shortText'].iloc[row.name] if df['richText'].iloc[row.name] in richTextFilter else combineStrings(df, row), axis=1)
+    saveAsJson(df, 'press42Combined.json')
+#Load the combined data
+if False:
+    path = getDataPath('press42Combined.json')
+    df = pd.read_json(path)
+
+#Filter the comb with stopwords, punctuation and lemma or load the combined cleaned data
+if False:
+    df['combClean'] = df.apply(lambda row: createStopPunctLemma(df['combText'].iloc[row.name]), axis=1)
+    saveAsJson(df, 'press42CombClean.json')
+else:
+    path = getDataPath('press42CombClean.json')
+    df = pd.read_json(path)
+    
+dfCombClean = df['combClean']
+    
+dictionary = gensim.corpora.Dictionary(dfCombClean)
+dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+bow_corpus = [dictionary.doc2bow(doc) for doc in dfCombClean]
+
+if False:
+    lda_model = gensim.models.LdaMulticore(bow_corpus, num_topics=10, id2word=dictionary, passes=2, workers=2)
+    saveModel(lda_model, 'lda_model')
+else:
+    lda_model = loadModel('lda_model')
+    
+    
+for idx, topic in lda_model.print_topics(-1):
+    print('Topic: {} \nWords: {}'.format(idx, topic))
 
 
-exampleText = df.iloc[0].richText
-
-
-#translate_table = dict((ord(char), None) for char in string.punctuation)   
-#exampleText.translate(translate_table)
-
-#df['train'] = df.apply(lambda row: df['shortText'].iloc[row.name] if df['richText'].iloc[row.name] in richTextFilter else combineStrings(df, row), axis=1)
-
-#dfMin = df['richText'].loc[df['richText'].str.len() < 120].unique()
-
-#dfStory = df.loc[df['richText'] == dfMin[2]]
-
-
-#print(dfMin[2].split('Foto')[0].rstrip())
-
-#print(exampleText)
 #Stop words nltk
 '''
 stopWords = set(stopwords.words('german'))
@@ -61,40 +108,4 @@ for word in words:
 
 print(wordsFiltered)
 '''
-#Stop words spacy
-# 
-'''
-doc = nlp(exampleText)
-tokens = [token.text for token in doc if not token.is_stop]
 
-print(tokens)
-
-doc = nlp(exampleText)
-tokens = []
-for token in doc:
-    if not isStopPunctSpace(token):
-        tokens.append(token.lemma_)
-
-print(tokens)
-
-#lemme
-
-import spacy
-import de_core_news_sm
-nlp = de_core_news_sm.load()
-
-
-mails=['Hallo. Ich spielte am frühen Morgen und ging dann zu einem Freund. Auf Wiedersehen', 'Guten Tag Ich mochte Bälle und will etwas kaufen. Tschüss']
-
-mails_lemma = []
-
-for mail in mails:
-     doc = nlp(mail)
-     result = ''
-     for token in doc:
-        result += token.lemma_
-        result += ' ' 
-     mails_lemma.append(result)
-     
-print(mails_lemma)
-'''
--- topic_modeling/spiders/QuodesSpider.py
+++ topic_modeling/spiders/QuodesSpider.py
@@ -51,67 +51,8 @@ class QuotesSpider(scrapy.Spider):
             'url': response.url
             }
  
-#        def xpath_get(response, _xpath):
-#            return response.xpath(_xpath).get().strip()
-#
-#        next_page = response.css('li.next a::attr(href)').get()
-#       if next_page is not None:
-#           yield response.follow(next_page, self.parse)
             
 #docker pull scrapinghub/splash
 #docker run -p 8050:8050 scrapinghub/splash
 #scrapy crawl quotes -o press.json
 
-'''         
-https://www.bundesregierung.de/breg-de/aktuelles/pressemitteilungen/buerokratieabbau-rechtsbereinigung-hebt-ueber-1-000-vorschriften-auf-758906
-
-'title':response.xpath('//span[contains(@class, "bpa-teaser-title-text-inner")]/text()').get(),
-'shortText': response.xpath('//div[contains(@class, "bpa-short-text")]/p/text()').getall(),
-'pressMessage': response.xpath('//li[contains(@class, "bpa-collection-item")]/text()').get(),
-'time': response.xpath('//span[contains(@class, "bpa-time")]/time/text()').get(),
-'author': response.xpath('//span[contains(@class, "bpa-dash")]/text()').get(),
-'richText': response.css('div.bpa-richtext *::text').getall()
-
-    def parse(self, response):
-        for _url in response.css("div.bpa-teaser-text-wrapper a::attr(href)").extract():
-            yield {
-                'link': _url
-            }
-                   
-            
-                        'author': response.xpath('//span[contains(@class, "bpa-dash")]/text()').get().encode('utf-8'),
-                        
-                        
-                                if self.count == 3:
-            return None;
-        
-        ++self.count
-        next_page = self.baseUrl+self.count
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-    
-            yield {
-            'title':self.xpath_get(reponse,'//span[contains(@class, "bpa-teaser-title-text-inner")]/text()'),
-            'shortText': self.list_to_string(response.xpath('//div[contains(@class, "bpa-short-text")]/p/text()').getall()),
-            'pressMessage': self.xpath_get(reponse,'//li[contains(@class, "bpa-collection-item")]/text()'),
-            'time': self.xpath_get(reponse,'//span[contains(@class, "bpa-time")]/time/text()'),
-            'author': self.xpath_get(reponse,'//span[contains(@class, "bpa-dash")]/text()'),
-            'richText': self.list_to_string(response.css('div.bpa-richtext *::text').getall()),
-            'url': response.url
-            }
-    
-    def list_to_string(_list):
-        return unicodedata.normalize("NFKD", ''.join(_list))
-    
-    def xpath_get(response, xpath):
-        return response.xpath(xpath).get().strip()
-    
-    
-              'title': xpath_get('//span[contains(@class, "bpa-teaser-title-text-inner")]/text()'),
-            'shortText': query_getall('//div[contains(@class, "bpa-short-text")]/p/text()', False),
-            'pressMessage': xpath_get('//li[contains(@class, "bpa-collection-item")]/text()'),
-            'time': xpath_get('//span[contains(@class, "bpa-time")]/time/text()'),
-            'author': xpath_get('//span[contains(@class, "bpa-dash")]/text()'),
-            'richText': query_getall('div.bpa-richtext *::text', True),
-            '''
